eb36846658ee84e5c71cd069a6801bccfea76c18
Luxi Zhao <lucyzhao.zlx@gmail.com>
2018-03-11 13:06:37 -0700

    can read all mrz info from passport

--- app/src/main/java/com/example/lucyzhao/votingapp/MainActivity.java
+++ app/src/main/java/com/example/lucyzhao/votingapp/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.lucyzhao.votingapp;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -13,9 +18,16 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,11 +37,13 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1100;
     private static final int TAKE_PICTURE = 23;
     TextView myTextView;
 
@@ -60,94 +74,90 @@ public class MainActivity extends AppCompatActivity {
         scanQRCode();
     }
 
+    public void scanPassport(View view) {
+        //startActivity(new Intent(this, NFCActivity.class));
+        new PassportInfoFragment().show(getFragmentManager(),"enterPassportInfo");
+    }
+
     private void scanQRCode() {
         //requestCameraPermission();
         Intent intent = new Intent(this, QRCodeActivity.class);
         startActivity(intent);
     }
 
-    /**
-     * Request user's permission for using camera if they haven't given
-     * it
-     */
-    private void requestCameraPermission() {
-        if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED) {
-            Log.v(TAG, "camera permission already granted");
-            takePicture();
-        }
-        else {
-            // try requesting the permission again.
-            Log.v(TAG, "requesting permission");
-            requestPermissions(
-                    new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
-        }
-    }
+//    /**
+//     * set the input fragment's switching behavior
+//     * @param fragment
+//     */
+//    private void createFragment(Fragment fragment) {
+//        fragment.setEnterTransition(new Slide(Gravity.END));
+//        fragment.setExitTransition(new Slide(Gravity.START));
+//        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+//
+//        // Replace whatever is in the fragment_container view with this fragment,
+//        // and add the transaction to the back stack if needed
+//        transaction.replace(R.id.activity_main, fragment);
+//        transaction.commit();
+//    }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        Log.v(TAG, "entering result");
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.v("tag", "trying to take picture");
-                    // permission was granted, take the picture
-                    takePicture();
-                } else {
-                    Log.v("tag", "camera access permission denied");
+    public static class PassportInfoFragment extends DialogFragment implements View.OnClickListener{
+//        private DatePicker birthDate;
+//        private DatePicker expiryDate;
+        private EditText birthDate;
+        private EditText expiryDate;
+        private EditText docNum;
+        private Button okBtn;
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState){
+            View fragment = inflater.inflate(R.layout.fragment_passport_info, container, false);
+            birthDate = fragment.findViewById(R.id.birthdate_edit_text);
+            expiryDate = fragment.findViewById(R.id.expirydate_edit_text);
+            docNum = fragment.findViewById(R.id.passport_num_edit_text);
+            okBtn = fragment.findViewById(R.id.save_info_ok_btn);
+            okBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    String birthDateStr = birthDate.getText().toString();
+                    String expiryDateStr = expiryDate.getText().toString();
+                    String docNumStr = docNum.getText().toString().toUpperCase();
+                    if(birthDateStr.length() != 6 || expiryDateStr.length() != 6 || docNumStr.isEmpty()) {
+                        Toast.makeText(getContext(), "Incorrect Info", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    SharedPreferences sharedPref = getActivity()
+                            .getSharedPreferences(getString(R.string.pref_file_key), Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString(getString(R.string.shared_pref_doc_num), docNumStr);
+                    editor.putString(getString(R.string.shared_pref_birthdate), birthDateStr);
+                    editor.putString(getString(R.string.shared_pref_expirydate), expiryDateStr);
+                    editor.apply(); //asynchronously save to pref
+                    Log.v(TAG,"saved to pref");
+                    dismiss();
                 }
-            }
+            });
+
+            return fragment;
+        }
+
+        //todo check for NULL
+        private String getDateStrFromPicker(DatePicker dp) {
+            SimpleDateFormat format = new SimpleDateFormat("yyMMdd");
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(dp.getYear(), dp.getMonth(), dp.getDayOfMonth());
+            Date date = calendar.getTime();
+            return format.format(date);
+        }
+
+        @Override
+        public void onClick(View view) {
+            dismiss();
         }
     }
 
-    private void takePicture() {
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, TAKE_PICTURE);
-    }
 
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            if (photo != null)
-                scanPicture(photo);
-            else {
-                Log.v(TAG, "photo null");
-            }
-        }
-    }
-
-    private void scanPicture(@NonNull Bitmap myBitmap) {
-
-
-        BarcodeDetector detector =
-                new BarcodeDetector.Builder(getApplicationContext())
-                        .setBarcodeFormats(Barcode.QR_CODE)
-                        .build();
-        if(!detector.isOperational()){
-            myTextView.setText("!!!!!!!!!!Could not set up the detector!");
-
-        }
-        else {
-            Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-
-            SparseArray<Barcode> barcodes = detector.detect(frame);
-            Log.v(TAG, "number of barcodes: " + barcodes.size());
-            if(barcodes.size() >= 1) {
-                Barcode thisCode = barcodes.valueAt(0);
-                myTextView.setText(thisCode.rawValue);
-            }
-            else {
-                myTextView.setText("Can't recognize QR Code");
-            }
-
-        }
-    }
 }
--- app/src/main/java/com/example/lucyzhao/votingapp/NFCActivity.java
+++ app/src/main/java/com/example/lucyzhao/votingapp/NFCActivity.java
@@ -1,14 +1,23 @@
 package com.example.lucyzhao.votingapp;
 
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
+import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,29 +28,40 @@ import net.sf.scuba.smartcards.CardServiceException;
 import org.jmrtd.BACKey;
 import org.jmrtd.PassportService;
 import org.jmrtd.lds.icao.DG1File;
-import org.jmrtd.lds.icao.DG2File;
-import org.jmrtd.lds.iso19794.FaceImageInfo;
-import org.jmrtd.lds.iso19794.FaceInfo;
+import org.jmrtd.lds.icao.MRZInfo;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
 public class NFCActivity extends AppCompatActivity {
     private static final String TAG = NFCActivity.class.getSimpleName();
     private static final String ISODEP_TECH_STRING = "android.nfc.tech.IsoDep";
     private NfcAdapter nfcAdapter;
+    private InfoAdapter infoAdapter;
+    private List<BioInfo> infoList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc);
-        Log.v(TAG, "in on create");
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        infoAdapter = new InfoAdapter(infoList);
+        RecyclerView infoRecycler = findViewById(R.id.info_recylerview);
+        infoRecycler.setLayoutManager(layoutManager);
+        infoRecycler.setAdapter(infoAdapter);
+
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         readPassport(getIntent());
+
     }
 
     @Override
@@ -56,13 +76,13 @@ public class NFCActivity extends AppCompatActivity {
 
         nfcAdapter.enableForegroundDispatch(this,
                 pendingIntent,
-                new IntentFilter[] {isodep},
-                new String[][] { new String[] { IsoDep.class.getName() } });
+                new IntentFilter[]{isodep},
+                new String[][]{new String[]{IsoDep.class.getName()}});
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         nfcAdapter.disableForegroundDispatch(this);
     }
 
@@ -74,60 +94,143 @@ public class NFCActivity extends AppCompatActivity {
     }
 
     private void readPassport(Intent intent) {
+        SharedPreferences sharedPref = this.getSharedPreferences(getString(R.string.pref_file_key), Context.MODE_PRIVATE);
+        String birthDate = sharedPref.getString(getString(R.string.shared_pref_birthdate), "");
+        String expDate = sharedPref.getString(getString(R.string.shared_pref_expirydate), "");
+        String docNum = sharedPref.getString(getString(R.string.shared_pref_doc_num), "");
+
+        Log.v(TAG, "opened shared prefs, birthdate " + birthDate + "exp date" + expDate);
+
         if (intent != null && NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             List<String> techArr = Arrays.asList(tag.getTechList());
-            if(techArr.contains(ISODEP_TECH_STRING)) {
-                IsoDep isoTag = IsoDep.get(tag);
-                CardService cardService = CardService.getInstance(isoTag);
-                try {
-                    //todo add ui for this
-                    BACKey bacKey = new BACKey("E28426441", "970109", "240821");
-                    PassportService passportService = new PassportService(cardService, 256, 224, false, true);
-                    passportService.open();
-                    passportService.sendSelectApplet(false);
-                    passportService.doBAC(bacKey);
-
-                    InputStream dg1Is = passportService.getInputStream(PassportService.EF_DG1);
-                    DG1File dg1File = new DG1File(dg1Is);
-                    String birthdate = dg1File.getMRZInfo().getDateOfBirth();
-                    Toast.makeText(this, "birthdate is" + birthdate, Toast.LENGTH_LONG).show();
-                    ((TextView) findViewById(R.id.birthdate)).setText(birthdate);
-                    ((TextView) findViewById(R.id.gender)).setText(dg1File.getMRZInfo().getGender().toString());
-                    ((TextView) findViewById(R.id.nationality)).setText(dg1File.getMRZInfo().getNationality());
-                    dg1Is.close();
-
-//                    InputStream dg2Is = passportService.getInputStream(PassportService.EF_DG2);
-//                    DG2File dg2File = new DG2File(dg2Is);
-//
-//                    List<FaceImageInfo> allFaceImageInfos = new ArrayList<>();
-//                    List<FaceInfo> faceInfos = dg2File.getFaceInfos();
-//                    for (FaceInfo faceInfo : faceInfos) {
-//                        allFaceImageInfos.addAll(faceInfo.getFaceImageInfos());
-//                    }
-//
-//                    if(allFaceImageInfos.size() > 0) {
-//                        Log.v(TAG, "images are found");
-//                        FaceImageInfo faceImageInfo = allFaceImageInfos.get(0);
-//                        Log.v(TAG, Integer.toString(faceImageInfo.getHairColor()));
-//                        Log.v(TAG, faceImageInfo.getEyeColor().name());
-//                    }
-//                    else {
-//                        Log.v(TAG, "no images found");
-//                    }
-
-                } catch (CardServiceException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-            else {
+            if (techArr.contains(ISODEP_TECH_STRING)) {
+                new ReadPassportTask(birthDate, expDate, docNum).execute(tag);
+            } else {
                 Toast.makeText(this, "please scan a passport", Toast.LENGTH_SHORT).show();
             }
 
         }
     }
+
+    private class ReadPassportTask extends AsyncTask<Tag, Integer, MRZInfo> {
+        private String birthDate, expiryDate, docNum;
+
+        ReadPassportTask(String birthdate, String expirydate, String docnum) {
+            this.birthDate = birthdate;
+            this.expiryDate = expirydate;
+            this.docNum = docnum;
+        }
+
+        @Override
+        protected MRZInfo doInBackground(Tag... params) {
+            IsoDep isoTag = IsoDep.get(params[0]);
+            CardService cardService = CardService.getInstance(isoTag);
+            try {
+                //todo add ui for this
+                //BACKey bacKey = new BACKey("E28426441", "970109", "240821");
+                BACKey bacKey = new BACKey(docNum, birthDate, expiryDate);
+                PassportService passportService = new PassportService(cardService, 256, 224, false, true);
+                passportService.open();
+                passportService.sendSelectApplet(false);
+                passportService.doBAC(bacKey);
+
+                InputStream dg1Is = passportService.getInputStream(PassportService.EF_DG1);
+                DG1File dg1File = new DG1File(dg1Is);
+                dg1Is.close();
+                return dg1File.getMRZInfo();
+            } catch (CardServiceException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected void onPostExecute(MRZInfo mrzInfo) {
+            if (mrzInfo == null) {
+                Toast.makeText(getApplicationContext(), "Authentication Failed", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            infoList.removeAll(infoList);
+            infoList.add(new BioInfo("Name",
+                    mrzInfo.getPrimaryIdentifier().replaceAll("\\W", "")
+                            + ", "
+                            + mrzInfo.getSecondaryIdentifier().replaceAll("\\W", "")));
+            infoList.add(new BioInfo("Nationality", mrzInfo.getNationality()));
+
+            SimpleDateFormat fromFormat = new SimpleDateFormat("yyMMdd");
+            SimpleDateFormat toFormat = new SimpleDateFormat("MMMM dd, yyyy", Locale.CANADA);
+            try {
+                Date birthDate = fromFormat.parse(mrzInfo.getDateOfBirth());
+                Date expiryDate = fromFormat.parse(mrzInfo.getDateOfExpiry());
+                infoList.add(new BioInfo("Birthdate", toFormat.format(birthDate)));
+                infoList.add(new BioInfo("Passport Expiry Date", toFormat.format(expiryDate)));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            infoList.add(new BioInfo("Personal Number", mrzInfo.getPersonalNumber()));
+            infoList.add(new BioInfo("Document Number", mrzInfo.getDocumentNumber()));
+            infoAdapter.notifyDataSetChanged();
+
+        }
+    }
+
+    private class BioInfo {
+        String tag;
+        String value;
+
+        BioInfo(String tag, String value) {
+            this.tag = tag;
+            this.value = value;
+        }
+    }
+
+    public class InfoAdapter extends RecyclerView.Adapter<InfoAdapter.ViewHolder> {
+        private List<BioInfo> passportInfo;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView tag, value;
+
+            public ViewHolder(View view) {
+                super(view);
+                tag = view.findViewById(R.id.info_tag);
+                value = view.findViewById(R.id.info_value);
+            }
+        }
+
+        public InfoAdapter(List<BioInfo> passportInfo) {
+            this.passportInfo = passportInfo;
+        }
+
+        @Override
+        public InfoAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.passport_info_item, parent, false);
+
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(InfoAdapter.ViewHolder holder, int position) {
+            holder.tag.setText(passportInfo.get(position).tag);
+            holder.value.setText(passportInfo.get(position).value);
+        }
+
+        @Override
+        public int getItemCount() {
+            return passportInfo.size();
+        }
+
+
+    }
+
 
 }
