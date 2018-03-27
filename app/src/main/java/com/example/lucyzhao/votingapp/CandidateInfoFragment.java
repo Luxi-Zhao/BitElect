f8568481c5ea4b671b83d4161ed24772accce664
Luxi Zhao <lucyzhao.zlx@gmail.com>
2018-03-27 16:21:29 -0700

    navigation drawer not doing anything

--- app/src/main/java/com/example/lucyzhao/votingapp/CandidateInfoFragment.java
+++ app/src/main/java/com/example/lucyzhao/votingapp/CandidateInfoFragment.java
@@ -1,9 +1,11 @@
 package com.example.lucyzhao.votingapp;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,8 +23,8 @@ import static com.example.lucyzhao.votingapp.Utils.COMM_CAND2_FN;
 import static com.example.lucyzhao.votingapp.Utils.COMM_CAND2_LN;
 import static com.example.lucyzhao.votingapp.Utils.VOTING_URL;
 
-public class CandidateInfoActivity extends AppCompatActivity {
-    private static final String TAG = CandidateInfoActivity.class.getSimpleName();
+public class CandidateInfoFragment extends Fragment {
+    private static final String TAG = CandidateInfoFragment.class.getSimpleName();
     private Button okBtn;
 
     private EditText cand1LastName;
@@ -31,34 +33,45 @@ public class CandidateInfoActivity extends AppCompatActivity {
     private EditText cand2FirstName;
 
 
+    public CandidateInfoFragment() {
+        // Required empty public constructor
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_candidate_info);
-        okBtn = findViewById(R.id.candidate_config_ok_btn);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View fragment = inflater.inflate(R.layout.fragment_candidate_info, container, false);
 
-        cand1LastName = findViewById(R.id.cand1ln);
-        cand1FirstName = findViewById(R.id.cand1fn);
-        cand2LastName = findViewById(R.id.cand2ln);
-        cand2FirstName = findViewById(R.id.cand2fn);
+        okBtn = fragment.findViewById(R.id.candidate_config_ok_btn);
+
+        cand1LastName = fragment.findViewById(R.id.cand1ln);
+        cand1FirstName = fragment.findViewById(R.id.cand1fn);
+        cand2LastName = fragment.findViewById(R.id.cand2ln);
+        cand2FirstName = fragment.findViewById(R.id.cand2fn);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cand1LastNameStr = cand1LastName.getText().toString();
+                String cand1FirstNameStr = cand1FirstName.getText().toString();
+                String cand2LastNameStr = cand2LastName.getText().toString();
+                String cand2FirstNameStr = cand2FirstName.getText().toString();
+
+                if (checkFields(cand1FirstNameStr, cand1LastNameStr, cand2FirstNameStr, cand2LastNameStr)) {
+                    sendConfig(cand1LastNameStr, cand1FirstNameStr, cand2LastNameStr, cand2FirstNameStr);
+                } else {
+                    Toast.makeText(fragment.getContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                }
+                Log.v(TAG, cand1FirstNameStr);
+            }
+        });
+        return fragment;
     }
 
-    public void submitConfig(View view) {
-        String cand1LastNameStr = cand1LastName.getText().toString();
-        String cand1FirstNameStr = cand1FirstName.getText().toString();
-        String cand2LastNameStr = cand2LastName.getText().toString();
-        String cand2FirstNameStr = cand2FirstName.getText().toString();
 
-        if (checkFields(cand1FirstNameStr, cand1LastNameStr, cand2FirstNameStr, cand2LastNameStr)) {
-            sendConfig(cand1LastNameStr, cand1FirstNameStr, cand2LastNameStr, cand2FirstNameStr);
-        } else {
-            Toast.makeText(getApplicationContext(), "Please fill in all fields", Toast.LENGTH_SHORT).show();
-        }
-        Log.v(TAG, cand1FirstNameStr);
-    }
 
     private void sendConfig(String cand1LN, String cand1FN, String cand2LN, String cand2FN) {
-        RequestQueue queue = Volley.newRequestQueue(this);
+        RequestQueue queue = Volley.newRequestQueue(getContext());
 
         String url = VOTING_URL
                 + COMM_CAND1_FN + "=" + cand1FN + "&"
